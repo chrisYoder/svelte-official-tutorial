105169a87ed881c595bdb47a6c7d1a6a0a8358f7
Chris <christophermyoder@gmail.com>
2020-04-21 10:10:20 -0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Button, { changeColor } from "./Button.svelte";
+  
+  export let name;
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+  }
+</style>
+
+<main>
+	<h1>Hello {name}</h1>
+	<Button />
+</main>
